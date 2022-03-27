26ff7cbccb2eba774817b7df9324a599d895c12e
Log <91562887+LogETH@users.noreply.github.com>
2022-03-26 19:15:29 -0700

    Update NFTRegisterRewardDistribution.sol

--- Past Commissions/NFTRegisterRewardDistribution.sol
+++ Past Commissions/NFTRegisterRewardDistribution.sol
@@ -6,7 +6,7 @@
 // You must indicate if changes were made in a reasonable manner, but not in any way that suggests I endorse you or your use.
 // If you remix, transform, or build upon the material, you must distribute your contributions under the same license as the original.
 // You may not apply legal terms or technological measures that legally restrict others from doing anything the license permits.
-// This TL;DR is solely an explaination and is not a representation of the licence.
+// This TL;DR is solely an explaination and is not a representation of the license.
 
 // By deploying this contract, you agree to the licence above and the terms and conditions that come with it.
 
