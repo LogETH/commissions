26af3b5174cc082ce84bc260c47ea45633bd0008
Log <91562887+LogETH@users.noreply.github.com>
2022-05-17 19:37:45 -0700

    Update Log.sol

--- Finished Commissions/Log.sol
+++ Finished Commissions/Log.sol
@@ -1,1 +1,106 @@
-this file is going to be a line of functions and statements that I just like having in every contract I make.
+// SPDX-License-Identifier: CC-BY-SA 4.0
+//https://creativecommons.org/licenses/by-sa/4.0/
+
+// TL;DR: The creator of this contract (@LogETH) is not liable for any damages associated with using the following code
+// This contract must be deployed with credits toward the original creator, @LogETH.
+// You must indicate if changes were made in a reasonable manner, but not in any way that suggests I endorse you or your use.
+// If you remix, transform, or build upon the material, you must distribute your contributions under the same license as the original.
+// You may not apply legal terms or technological measures that legally restrict others from doing anything the license permits.
+// This TL;DR is solely an explaination and is not a representation of the license.
+
+// By deploying this contract, you agree to the license above and the terms and conditions that come with it.
+
+pragma solidity >=0.8.0 <0.9.0;
+
+contract InsertNiceName{
+
+//// This contract is a template that I made for easy coding.
+
+    // Put instructions on how to setup the completed contract here:
+
+    // Step 1: Deploy the contract
+    // Step 2: Microwave some instant ramen
+    // Step 3: Put on your programming accessories 
+    // Step 4: Make blockchain go beep boop and code
+
+//// Commissioned by XXXX#1234 on X/XX/XXXX
+
+    // the constructor that activates when you deploy the contract, this is where you change settings before deploying.
+
+    constructor(){
+
+        
+    }
+
+
+//////////////////////////                                                          /////////////////////////
+/////////////////////////                                                          //////////////////////////
+////////////////////////            Variables that this contract has:             ///////////////////////////
+///////////////////////                                                          ////////////////////////////
+//////////////////////                                                          /////////////////////////////
+
+
+//// Special Variables go here:
+
+    ERC20 Token;
+
+//// All the Variables that this contract uses
+
+    mapping(address => uint) ExampleVar;
+    uint ExampleNum;
+
+//////////////////////////                                                              /////////////////////////
+/////////////////////////                                                              //////////////////////////
+////////////////////////             Visible functions this contract has:             ///////////////////////////
+///////////////////////                                                              ////////////////////////////
+//////////////////////                                                              /////////////////////////////
+
+
+    // Put visible functions (Buttons that people press) here:
+
+
+//////////////////////////                                                              /////////////////////////
+/////////////////////////                                                              //////////////////////////
+////////////////////////      Internal and external functions this contract has:      ///////////////////////////
+///////////////////////                                                              ////////////////////////////
+//////////////////////                                                              /////////////////////////////
+
+
+    // Put internal functions here: (There should NOT be any msg.sender or tx.origin in here)
+
+
+//////////////////////////                                                              /////////////////////////
+/////////////////////////                                                              //////////////////////////
+////////////////////////                 Functions used for UI data                   ///////////////////////////
+///////////////////////                                                              ////////////////////////////
+//////////////////////                                                              /////////////////////////////
+
+
+    
+///////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+//// Additional functions that are not part of the core functionality, if you add anything, please add it here ////
+///////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+/*
+    function something() public {
+
+        blah blah blah blah;
+    }
+*/
+
+
+
+}
+    
+//////////////////////////                                                              /////////////////////////
+/////////////////////////                                                              //////////////////////////
+////////////////////////      Contracts that this contract uses, contractception!     ///////////////////////////
+///////////////////////                                                              ////////////////////////////
+//////////////////////                                                              /////////////////////////////
+
+interface ERC20{
+    function transferFrom(address, address, uint256) external;
+    function transfer(address, uint256) external;
+    function balanceOf(address) external view returns(uint);
+    function decimals() external view returns (uint8);
+}
