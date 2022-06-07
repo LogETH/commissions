42d64be503dfdc46cc50c55ca4c696c018b9f892
Log <91562887+LogETH@users.noreply.github.com>
2022-06-07 16:39:52 -0700

    Update SimpleDAO.sol

--- Active Commissions/SimpleDAO.sol
+++ Active Commissions/SimpleDAO.sol
@@ -6,27 +6,30 @@
 // You must indicate if changes were made in a reasonable manner, but not in any way that suggests I endorse you or your use.
 // If you remix, transform, or build upon the material, you must distribute your contributions under the same license as the original.
 // You may not apply legal terms or technological measures that legally restrict others from doing anything the license permits.
-// This TL;DR is solely an explaination and is not a representation of the licence.
+// This TL;DR is solely an explaination and is not a representation of the license.
 
-// By deploying this contract, you agree to the licence above and the terms and conditions that come with it.
+// By deploying this contract, you agree to the license above and the terms and conditions that come with it.
 
-pragma solidity >=0.7.0 <0.9.0;
+pragma solidity >=0.8.0 <0.9.0;
 
-//// What is this contract? 
+contract SimpleDAO{
 
 //// This contract is an ERC20 token that has a DAO module attached to it
 //// People can vote on Proposals and stuff 
 //// You cannot send or use tokens that are currently being used to vote btw.
 
+    // How to setup:
+
+    // Step 1: Deploy the contract
+    // Step 2: That's pretty much it, all the tokens are sent to you, so go wild.
+
 //// Commissioned by WojtekBB#8959 on 6/7/2022
 
-contract TokenWithDAO {
-
-//// Before you deploy the contract, make sure to change these parameters to what you want
+    // the constructor that activates when you deploy the contract, this is where you change settings before deploying.
 
     constructor(){
 
-        balances[msg.sender] = 2000000*10**18;
+        balanceOf[msg.sender] = 2000000*10**18;
         totalSupply = 2000000*10**18;
         name = "LOG token";
         decimals = 18;
@@ -38,40 +41,64 @@ contract TokenWithDAO {
         admin = msg.sender;             // The admin is able to change everything above... but this is a DAO, do you really want that?
     }
 
-    mapping (address => uint256) public balances; 
-    mapping (address => uint256) public FrozenTokens;
-    mapping (address => mapping (address => uint256)) public allowed;
 
+//////////////////////////                                                          /////////////////////////
+/////////////////////////                                                          //////////////////////////
+////////////////////////            Variables that this contract has:             ///////////////////////////
+///////////////////////                                                          ////////////////////////////
+//////////////////////                                                          /////////////////////////////
+
+//// All the Variables that this contract uses
+
+    mapping (address => uint256) public balanceOf; 
+    mapping (address => uint256) public FrozenTokens;
+    mapping (address => mapping (address => uint256)) public allowance;
+
+    uint Nonce;
+    uint MinProp;
+
+
+
+    string public name;
+    string public symbol;
+    address public admin;
+    uint8 public decimals;
+    uint public totalSupply;
+    uint public MaxVotingTime;
+
+
+    mapping (uint => uint) VoteTime;
+    mapping (address => bool) Voted;
+    mapping (uint => address) Proposal;
+    mapping (address => uint) VoteTokens;
+    mapping (address => bool) ProposalApproved;
+    mapping (uint => mapping(bool => uint)) VoteBox;
+    mapping (uint => mapping(uint => address)) userNonce;
+
+    
     event Transfer(address indexed _from, address indexed _to, uint256 _value);
     event Approval(address indexed _owner, address indexed _spender, uint256 _value);
 
-    string public name;
-    uint8 public decimals;
-    string public symbol;
-    uint public totalSupply;
-    address public admin;
-    uint public MaxVotingTime;
-    uint Nonce;
-
-    uint MinProp;
-    mapping (uint => address) Proposal;
-    mapping (address => bool) ProposalApproved;
-    mapping (uint => mapping(bool => uint)) VoteBox;
-    mapping (address => bool) Voted;
-    mapping (address => uint) VoteTokens;
-    mapping (uint => mapping(uint => address)) userNonce;
-    mapping (uint => uint) VoteTime;
-
-    function EditMinProp(uint HowMuch) public {
+    modifier adminOnly(){
 
         require(msg.sender == admin);
+        _;
+    }
+
+
+//////////////////////////                                                              /////////////////////////
+/////////////////////////                                                              //////////////////////////
+////////////////////////             Visible functions this contract has:             ///////////////////////////
+///////////////////////                                                              ////////////////////////////
+//////////////////////                                                              /////////////////////////////
+
+
+    function EditMinProp(uint HowMuch) public adminOnly{
 
         MinProp = HowMuch;
     }
 
-    function EditMaxVotingTime(uint HowMuch) public {
-
-        require(msg.sender == admin);
+    function EditMaxVotingTime(uint HowMuch) public adminOnly{
 
         MinProp = HowMuch;
     }
@@ -79,49 +106,37 @@ contract TokenWithDAO {
 
     function transfer(address _to, uint256 _value) public returns (bool success) {
 
-        require(balances[msg.sender] - FrozenTokens[msg.sender] >= _value, "You can't send more tokens than you have");
+        require(balanceOf[msg.sender] - FrozenTokens[msg.sender] >= _value, "You can't send more tokens than you have avalable");
 
-
-        balances[msg.sender] -= _value;
-        balances[_to] += _value;
+        balanceOf[msg.sender] -= _value;
+        balanceOf[_to] += _value;
         emit Transfer(msg.sender, _to, _value);
         return true;
     }
 
     function transferFrom(address _from, address _to, uint256 _value) public returns (bool success) {
 
-        require(balances[_from] - FrozenTokens[msg.sender] >= _value && allowed[_from][msg.sender] >= _value, "You can't send more tokens than you have or the approval isn't enough");
+        require(balanceOf[_from] - FrozenTokens[msg.sender] >= _value && allowance[_from][msg.sender] >= _value, "You can't send more tokens than you have avalable or the approval isn't enough");
 
-        balances[_to] += _value;
-        balances[_from] -= _value;
-        allowed[_from][msg.sender] -= _value;
+        balanceOf[_to] += _value;
+        balanceOf[_from] -= _value;
+        allowance[_from][msg.sender] -= _value;
 
         emit Transfer(_from, _to, _value);
         return true;
     }
 
-    function balanceOf(address _owner) public view returns (uint256 balance) {
-
-        return balances[_owner];
-    }
-
     function approve(address _spender, uint256 _value) public returns (bool success) {
 
-        allowed[msg.sender][_spender] = _value;
-
+        allowance[msg.sender][_spender] = _value;
         emit Approval(msg.sender, _spender, _value); 
         return true;
-    }
-
-    function allowance(address _owner, address _spender) public view returns (uint256 remaining) {
-
-        return allowed[_owner][_spender];
     }
 
     function ProposeVote(address ProposalAddress) public {
 
         Nonce ++;
-        require(balanceOf(msg.sender) >= MinProp*totalSupply/100, "You don't have enough tokens to make a proposal");
+        require(balanceOf[msg.sender] >= MinProp*totalSupply/100, "You don't have enough tokens to make a proposal");
         VoteTime[Nonce] = block.timestamp;
 
         Proposal[Nonce] = ProposalAddress;
@@ -132,29 +147,24 @@ contract TokenWithDAO {
         require(Voted[msg.sender] = false, "You already voted on this proposal!");
         require(block.timestamp - VoteTime[ProposalID] < MaxVotingTime, "Voting time is over!");
 
-        VoteBox[ProposalID][TrueOrFalse] + HowManyTokens;
         Voted[msg.sender] = true;
-        VoteTokens[msg.sender] + HowManyTokens;
+        VoteTokens[msg.sender] += HowManyTokens;
+        VoteBox[ProposalID][TrueOrFalse] += HowManyTokens;
 
         FrozenTokens[msg.sender] += HowManyTokens;
-        
     }
 
     function FinalizePropResult(uint ProposalID) public {
 
         require(block.timestamp - VoteTime[ProposalID] > MaxVotingTime, "Voting has not ended yet!");
+        if(VoteBox[ProposalID][true] > VoteBox[ProposalID][false]){ProposalApproved[Proposal[ProposalID]] = true;}
 
-        if(VoteBox[ProposalID][true] > VoteBox[ProposalID][false]){
+        uint x; 
 
-            ProposalApproved[Proposal[ProposalID]] = true;
-        }
+        while(userNonce[ProposalID][x] != address(0)){
 
-        uint i;
-
-        while(userNonce[ProposalID][i] != address(0)){
-
-            FrozenTokens[userNonce[ProposalID][i]] -= VoteTokens[userNonce[ProposalID][i]];
-            i ++;
+            FrozenTokens[userNonce[ProposalID][x]] -= VoteTokens[userNonce[ProposalID][x]];
+            x ++;
         }
 
     }
@@ -171,9 +181,14 @@ contract TokenWithDAO {
         token.approve(WhatAddress, HowManyTokens);
     }
 }
+    
+//////////////////////////                                                              /////////////////////////
+/////////////////////////                                                              //////////////////////////
+////////////////////////      Contracts that this contract uses, contractception!     ///////////////////////////
+///////////////////////                                                              ////////////////////////////
+//////////////////////                                                              /////////////////////////////
 
 interface VoteContract{
-
     function Execute() external;
 }
 
