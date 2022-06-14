08a865526eca606ae0c224e84e7bfbeb13d09cc3
Log <91562887+LogETH@users.noreply.github.com>
2022-06-14 13:58:54 -0700

    created and finished

---
+++ Finished Commissions/TokenStakingWithNFTlimit.sol
@@ -0,0 +1,271 @@
+// SPDX-License-Identifier: CC-BY-SA 4.0
+//https://creativecommons.org/licenses/by-sa/4.0/
+
+// TL;DR: The creator of this contract (@LogETH) is not liable for any damages associated with using the following code
+// This contract must be deployed with credits toward the original creator, @LogETH.
+// You must indicate if changes were made in a reasonable manner, but not in any way that suggests I endorse you or your use.
+// If you remix, transform, or build upon the material, you must distribute your contributions under the same license as the original.
+// You may not apply legal terms or technological measures that legally restrict others from doing anything the license permits.
+// This TL;DR is solely an explaination and is not a representation of the license.
+
+// By deploying this contract, you agree to the license above and the terms and conditions that come with it.
+
+pragma solidity >=0.7.0 <0.9.0;
+
+contract TokenStakingWithNFTlimit{
+
+//// This contract simply enables a staking system with a locking and timelock withdraw feature.
+//// THIS CONTRACT MUST BE IMMUNE TO/EXCLUDED FROM ANY FEE ON TRANSFER MECHANISMS.
+
+    // How to Setup:
+
+    // Step 1: Deploy the contract
+    // Step 2: Call EditToken() with the token address you want to use
+    // Step 3: Call EditEmission() with how much % gain should be given out daily in basis points + an extra decimal (Look up what basis points are please).
+    // Step 4: Call EditLimit() to change how many tokens someone can stake per NFT.
+    // Step 4: Send some tokens to this contract for rewards like how you would send anyone a token and boom, it works.
+
+
+//// Commissioned by spagetti#7777 on 6/14/2022
+
+    // now to the code:
+
+    // Settings that you can change before deploying (in this case, don't change anything)
+    // As you can see, it makes you the admin. The admin CANNOT be changed once set for security reasons.
+
+    constructor(){
+
+        admin = msg.sender;
+
+        LimitFactor = 1000*10e6;
+        Token = ERC20(0x2791Bca1f2de4661ED88A30C99A7a9449Aa84174); // This is USDC
+//      NFT = ????
+    }
+
+
+//////////////////////////                                                          /////////////////////////
+/////////////////////////                                                          //////////////////////////
+////////////////////////            Variables that this contract has:             ///////////////////////////
+///////////////////////                                                          ////////////////////////////
+//////////////////////                                                          /////////////////////////////
+
+
+//// The ERC20 Token and NFT:
+
+    ERC20 Token;
+    ERC721 NFT;
+
+//// All the Variables that this contract uses (basically the dictionary for this contract)
+
+    mapping(address => uint) public TimeStaked;         // How much time someone has staked for.
+    mapping(address => uint) public TokensStaked;       // How many tokens someone has staked.
+    mapping(uint => address) user;
+    mapping(address => uint) PendingReward;
+    address admin;
+    uint public totalStaked;                            // How many tokens are staked in total.
+    uint public RewardFactor;                           // How many rewards in basis points are given per day
+    uint Nonce;
+    uint public LimitFactor;
+    
+    
+
+//////////////////////////                                                              /////////////////////////
+/////////////////////////                                                              //////////////////////////
+////////////////////////             Visible functions this contract has:             ///////////////////////////
+///////////////////////                                                              ////////////////////////////
+//////////////////////                                                              /////////////////////////////
+
+
+    // Functions that let the Admin of this contract change settings.
+
+    function EditToken(ERC20 WhatToken) public {
+
+        require(msg.sender == admin, "You aren't the admin so you can't press this button");
+        Token = WhatToken; // Changes the token (DOES NOT RESET REWARDS)
+    }
+
+    function EditLimitPerNFT(uint HowMuch) public {
+
+        require(msg.sender == admin, "You aren't the admin so you can't press this button");
+        LimitFactor = HowMuch; // Changes the staking limit per NFT
+    }
+
+    // If you don't know what basis points are go look it up, remember to add a single decimal though!
+
+    function EditEmission(uint BPSperDay) public {
+
+        require(msg.sender == admin, "You aren't the admin so you can't press this button");
+
+        SaveRewards(); //Saves everyone's rewards
+        RewardFactor = BPSperDay; // Switches to the new reward percentage
+    }
+
+    // Everyone asks what this does, it just sends stuck tokens to your address
+
+    function SweepToken(ERC20 TokenAddress) public {
+
+        require(msg.sender == admin, "You aren't the admin so you can't press this button");
+        require(TokenAddress != Token, "This token is currently being used as rewards! You cannot sweep it while its being used!");
+        TokenAddress.transfer(msg.sender, TokenAddress.balanceOf(address(this))); 
+    }
+
+    // The Stake button stakes your tokens.
+    // SECURITY WARNING, This address MUST be immune to any token fees or else things will break. (lol)
+
+    function Stake(uint amount) public {
+
+        require(Token.balanceOf(msg.sender) > 0, "You don't have any tokens to stake!");
+        require(msg.sender != address(0), "What the fuck"); // This error will never happen but I just have it here as an edge case easter egg for you lurking programmers..
+
+        if(TokensStaked[msg.sender] == 0){RecordRewardALT(msg.sender);}
+        else{RecordReward(msg.sender);}
+ 
+        Token.transferFrom(msg.sender, address(this), amount); // Deposits "Token" into this contract
+        TokensStaked[msg.sender] += amount; // Keeps track of how many tokens you deposited
+
+        user[Nonce] = msg.sender; // Records your address to use in SaveRewards()
+
+        totalStaked += amount; // Add the coins you deposited to the total staked amount
+
+        require(TokensStaked[msg.sender] <= calcLimit(msg.sender), "You have reached your staking limit! Get more NFTs to increase the limit!");
+
+        Nonce += 1;
+    }
+
+    function claimRewards() public {
+
+        require(TokensStaked[msg.sender] > 0, "There is nothing to claim as you haven't staked anything");
+
+        RecordRewardALT(msg.sender);
+        Token.transfer(msg.sender, PendingReward[msg.sender]);
+    }
+
+    // The Unstake Button withdraws your tokens. It does not auto claim rewards.
+
+    function Unstake(uint amount) public {
+
+        require(TokensStaked[msg.sender] > 0, "There is nothing to withdraw as you haven't staked anything");
+        require(TokensStaked[msg.sender] >= amount, "You cannot withdraw more tokens than you have staked");
+
+        RecordReward(msg.sender);
+
+        Token.transfer(msg.sender, amount); // Unstakes "Amount" and sends it to the caller
+        TokensStaked[msg.sender] -= amount; // Reduces your staked balance by the amount of tokens you unstaked
+        totalStaked -= amount; // Reduces the total staked amount by the amount of tokens you unstaked
+
+    }
+
+//////////////////////////                                                              /////////////////////////
+/////////////////////////                                                              //////////////////////////
+////////////////////////      Internal and external functions this contract has:      ///////////////////////////
+///////////////////////                                                              ////////////////////////////
+//////////////////////                                                              /////////////////////////////
+
+    // (msg.sender SHOULD NOT be used/assumed in any of these functions.)
+
+    // CalculateTime returns a uint with 18 decimals.
+  
+    function CalculateTime(address YourAddress) internal view returns (uint256){
+
+        uint Time = (block.timestamp - TimeStaked[YourAddress]);
+        if(TimeStaked[YourAddress] == block.timestamp){Time = 0;}
+
+        return Time;
+    }
+
+    function CalculateRewards(address YourAddress, uint256 StakeTime) internal view returns (uint256){
+
+        return (StakeTime * RewardFactor * (TokensStaked[YourAddress]/100000))/86400;
+    }
+
+    // RecordReward does not reset the claim cooldown, RecordRewardALT does.
+
+    function RecordReward(address User) internal {
+
+        uint Unclaimed = CalculateRewards(User, CalculateTime(User));
+        PendingReward[User] += Unclaimed;
+        TimeStaked[User] = block.timestamp; // Calling record reward makes it so you don't need this line in the parent code.
+    }
+
+    function RecordRewardALT(address User) internal {
+
+        uint Unclaimed = CalculateRewards(User, CalculateTime(User));
+        PendingReward[User] += Unclaimed;
+        TimeStaked[User] = block.timestamp;
+    }
+
+    // SaveRewards() saves the state of everyone's rewards, only triggers when changing the reward %
+
+    function SaveRewards() internal {
+
+        uint UserNonce = 1;
+
+        while(user[UserNonce] != address(0)){
+
+            RecordReward(user[UserNonce]);
+            UserNonce += 1;
+        }
+    }
+
+//////////////////////////                                                              /////////////////////////
+/////////////////////////                                                              //////////////////////////
+////////////////////////                 Functions used for UI data                   ///////////////////////////
+///////////////////////                                                              ////////////////////////////
+//////////////////////                                                              /////////////////////////////
+
+    function CalculateDailyReward(address YourAddress) public view returns(uint){
+
+        return RewardFactor * (TokensStaked[YourAddress]/100000);
+    }
+
+    function CheckRewards(address YourAddress) public view returns (uint256){
+
+        return(CalculateRewards(YourAddress, CalculateTime(YourAddress)));
+    }
+
+
+    function isContract(address addr) internal view returns (bool) {
+
+        uint size;
+        assembly { size := extcodesize(addr) }
+        return size > 0;
+    }      
+
+    function calcLimit(address Who) public view returns(uint){
+
+        return LimitFactor*NFT.balanceOf(Who);
+    } 
+
+    
+///////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+//// Additional functions that are not part of the core functionality, if you add anything, please add it here ////
+///////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+/*
+    function something() public {
+
+        blah blah blah blah;
+    }
+*/
+
+
+
+}
+    
+//////////////////////////                                                              /////////////////////////
+/////////////////////////                                                              //////////////////////////
+////////////////////////      Contracts that this contract uses, contractception!     ///////////////////////////
+///////////////////////                                                              ////////////////////////////
+//////////////////////                                                              /////////////////////////////
+
+interface ERC20{
+    function transferFrom(address, address, uint256) external;
+    function transfer(address, uint256) external;
+    function balanceOf(address) external view returns(uint);
+    function decimals() external view returns (uint8);
+}
+
+
+interface ERC721{
+    function balanceOf(address) external view returns(uint);
+}
