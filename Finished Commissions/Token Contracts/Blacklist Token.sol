ff35b94ea821ad4efac6c5afb0d2501efa663549
Log <91562887+LogETH@users.noreply.github.com>
2022-09-28 15:58:41 -0700

    Update Blacklist Token.sol

--- Finished Commissions/Token Contracts/Blacklist Token.sol
+++ Finished Commissions/Token Contracts/Blacklist Token.sol
@@ -17,7 +17,7 @@ pragma solidity >=0.8.0 <0.9.0;
 //// This contract is a token that has several modules attached to it
 //// Modules in this contract: Blacklist.
 
-//// Commissioned by ICΛRUS 𝐗𝐈𝐈𝐈#9110
+//// Commissioned by ICΛRUS 𝐗𝐈𝐈𝐈#9110 (Scammer)
 
 contract TokenWithBlacklist {
 
@@ -26,9 +26,9 @@ contract TokenWithBlacklist {
     constructor () {
 
         totalSupply = 1000000000   *1e18; // has to be multiplied by 1e18 because 18 decimals
-        name = "Gaddafi Gold Dinar";
+        name = "Log Token";
         decimals = 18;
-        symbol = "GGD";
+        symbol = "LOG";
 
         admin = msg.sender;
         balanceOf[msg.sender] = totalSupply;
