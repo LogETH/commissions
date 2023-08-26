c9490ed3cb45e4bb1fb2fdb6f4fab695c204851f
Log <91562887+LogETH@users.noreply.github.com>
2023-08-25 17:05:35 -0700

    Finish RepeatingLotteryBase.sol
    Finished code

--- Active Commissions/RepeatingLotteryBase.sol
+++ Active Commissions/RepeatingLotteryBase.sol
@@ -17,11 +17,8 @@ contract RepeatingLottery{
 //// This contract is a lottery that repeats itself
 
     // Instructions on how to setup the contract:
+    // https://docs.google.com/document/d/1dA6kW0114xwe_flailso4VwDyUrCXknFjhqKydbBT5U/edit
 
-    // Step 1: Deploy the contract
-    // Step 2: Microwave some instant ramen
-    // Step 3: Put on your programming accessories 
-    // Step 4: Make blockchain go beep boop and code
 
 //// Commissioned by @kommissar29 on 8/18/2023
 
@@ -29,22 +26,32 @@ contract RepeatingLottery{
 
     constructor(){
 
-        USDT = ERC20(0x0000000000000000000000000000000000000000); // What token this contract uses
-        admin = msg.sender;
+        USDT = ERC20(0x3028335Bb5fa053805ACa3B89b8E091068d80a4E);   // What token this contract uses
+        admin = msg.sender;                                         // Makes you the admin
 
-        raffleDuration = 1 weeks; // How long the raffle should take
-        limited = false; // Does this raffle allow contributing more than once per raffle?
+        raffleDuration = 60 seconds;                                // How long the raffle should take
 
-        cost = 1*1e18; // How much a single contrubition should cost, 1*1e18 = 1 USDT, change the first number to edit the amount.
+        limited = false;                                            // Does this raffle restrict contributing to once per raffle?
 
-        // Wallets where the fees go to:
+        cost = 10*1e18; // How much a single contrubition costs, 10*1e18 = 10 USDT. Change the first number to edit the amount.
 
-        taxWallet[1] = 0x0000000000000000000000000000000000000000; // 23.3%
-        taxWallet[2] = 0x0000000000000000000000000000000000000000; // 23.3%
-        taxWallet[3] = 0x0000000000000000000000000000000000000000; // 23.3%
-        taxWallet[4] = 0x0000000000000000000000000000000000000000; // 20.1%
+        //// Wallets where the fees go to:
 
-        refAddress = Referral(0x0000000000000000000000000000000000000000);
+        taxWallet[0] = 0x0000000000000000000000000000000000000000;  // 23.3%
+        taxWallet[1] = 0x0000000000000000000000000000000000000000;  // 23.3%
+        taxWallet[2] = 0x0000000000000000000000000000000000000000;  // 23.3%
+        taxWallet[3] = 0x0000000000000000000000000000000000000000;  // 20.1%
+
+        //// Contract that handles referrals
+
+        refAddress = Ref(0xf30D5a37544A768FBFCb2C6b91Bfcf0d8b6E5CC5); // referral contract address
+        refMultiplier = 1;                                          // How many referral entries should one contribution give?
+
+        //// Chainlink variables, chainlink should tell you what to put here when you make a subscription, the values here are for sepolia testnet.
+
+        s_subscriptionId = 4757;
+        vrfCoordinator = 0x8103B0A8A00be2DDC778e6e7eaa21791Cd364625;
+        s_keyHash = 0x474e34a077df58807dbe9c96d3c009b23b3c6d0cce433e59bbf5b34f823bc56c;
     }
 
 
@@ -55,36 +62,51 @@ contract RepeatingLottery{
 //////////////////////                                                          /////////////////////////////
 
 
-//// Special Variables go here:
+//// Contract variables
 
-    ERC20 USDT;
-    Referral refAddress;
+    ERC20 public USDT;
+    Ref public refAddress;
 
-//// All the Variables that this contract uses
+//// Mappings
 
     uint public raffleNonce; // What number raffle we are on, starts at 0, you can use previous numbers to look up data about previous raffles
 
     mapping(uint => mapping (address => bool)) public hasContributed; // Who has contributed to the raffle
-    mapping(uint => mapping (uint => address)) public list;           // Who is in the raffle
+    mapping(uint => mapping (uint => address)) public list;           // List of addresses in the raffle
     mapping(uint => address) public winner;                           // The winners of the raffles
     mapping(uint => uint) public endTime;                             // The time the raffle's winner is drawn
     mapping(uint => uint) internalPot;                                // The current pot with fees added
+    mapping(uint => address) taxWallet;                               // A list of wallets where the fees go to
 
+//// Single variables
 
+    address public admin;                                       // The address that is allowed to use admin functions
+
+    bool public open;                                           // Is the raffle initalized?
+    bool public limited;                                        // Whether the raffle allows for multiple entries or not.
+    bool rolling;                                               // If the winner is currently being drawn, prevents the contract from accedentally drawing twice.
 
     uint public raffleDuration;                                 // How long the raffle lasts for
-    address public admin;                                       // The address that is allowed to use admin functions
     uint public nonce;                                          // How many contributions the current raffle has received, starts at 0 so add 1 to get the true amount.
     uint public cost;                                           // How much it costs to contribute to the pot
-    bool open;
-    bool limited;                                               // Whether the raffle allows for multiple entries or not.
-    address[] taxWallet;
-
+    uint public refMultiplier;                                  // How many entries into the referral pot should one referral give?
+    uint remTime;                                               // The remaining amount of time if the raffle is paused.
     
 //// Modifiers
 
-    modifier onlyAdmin{require(msg.sender == admin,"Not Authorized");_;}
-    modifier requireOpen{require(open,"Not Initalized");_;}
+    modifier requireNotRolling  {require(!rolling,           "Cannot use while rolling");_;}
+    modifier onlyAdmin          {require(msg.sender == admin,"Not Authorized");_;}
+    modifier requireClosed      {require(!open,              "Not Closed");_;}
+    modifier requireOpen        {require(open,               "Not Open");_;}
+
+//// Chainlink variables 
+
+    uint64 s_subscriptionId;
+    address vrfCoordinator;
+    bytes32 s_keyHash;
+    uint32 public callbackGasLimit = 200000;
+    uint16 requestConfirmations = 3;
+    uint32 numWords = 1;
 
 
 //////////////////////////                                                              /////////////////////////
@@ -98,17 +120,36 @@ contract RepeatingLottery{
 
     // Begins the raffle
 
-    function initializeRaffle() public onlyAdmin{
+    function initializeRaffle() public onlyAdmin requireClosed{
 
+        endTime[raffleNonce] = block.timestamp + raffleDuration;
         open = true;
-        
     }
 
-    // Lets someone contribute
+    function pauseRaffle() public onlyAdmin requireOpen requireNotRolling{
 
-    function contribute(uint HowManyTimes) public requireOpen{
+        open = false;
+        remTime = endTime[raffleNonce] - block.timestamp;
+    }
+
+    function resumeRaffle() public onlyAdmin requireClosed{
+
+        open = true;
+        endTime[raffleNonce] = remTime + block.timestamp;
+    }
+
+    function changeCallbackGasLimit(uint32 howmuch) public onlyAdmin{
+
+        callbackGasLimit = howmuch;
+    }
+
+    // Lets someone contribute, put 0x0000...0000 for no referral address.
+
+    function contribute(uint HowManyTimes, address referralAddress) public requireOpen{
 
         require(HowManyTimes != 0, "Cannot be zero");
+        require(referralAddress != msg.sender, "You cannot refer yourself");
+        require(!rolling, "Cannot contribute when the winner is being rolled");
 
         // If set, restrict contributing once per address for each raffle.
         if(limited){require(!hasContributed[raffleNonce][msg.sender] && HowManyTimes == 1, "You can only contribute once each raffle");}
@@ -117,34 +158,108 @@ contract RepeatingLottery{
 
         // enter the msg.sender into the raffle as many times as they paid for.
 
-        for(uint i; i>HowManyTimes; i++){
+        for(uint i; i < HowManyTimes; i++){
 
             list[raffleNonce][nonce] = msg.sender;                       // List the msg.sender as a contributor
+            nonce++;                                                     // move up the list by one
         }
 
         if(limited){hasContributed[raffleNonce][msg.sender] = true;}     // If set, keep track if the msg.sender has bought a ticket.
 
         internalPot[raffleNonce] += cost*HowManyTimes;
 
-        nonce++;
+        if(referralAddress != address(0)){
+
+            refAddress.enterReferral(referralAddress, HowManyTimes*refMultiplier);
+        }
     }
 
-    function drawWinner() public requireOpen{
+    function drawWinner() public requireOpen requireNotRolling{
 
         require(block.timestamp > endTime[raffleNonce], "The raffle has not ended yet");
 
-        winner[raffleNonce] = rollWinner(); // roll the winner
+        //// If there are no contributions, restart the raffle
+
+        if(nonce == 0){
+
+            raffleNonce ++;
+            nonce = 0;
+            endTime[raffleNonce] = block.timestamp + raffleDuration;
+            rolling = false;
+
+            return;
+        }
+
+        //// rolling bool so someone cannot call this while the random number is being rolled
+
+        rolling = true;
 
         // Handle the fees
 
         uint fees = (internalPot[raffleNonce]/100)*30; // 30% of the pot is taken as fees
 
+        USDT.transfer(taxWallet[0], (fees/1000)*233);
         USDT.transfer(taxWallet[1], (fees/1000)*233);
         USDT.transfer(taxWallet[2], (fees/1000)*233);
-        USDT.transfer(taxWallet[3], (fees/1000)*233);
-        USDT.transfer(taxWallet[4], (fees/1000)*201);
+        USDT.transfer(taxWallet[3], (fees/1000)*201);
 
         USDT.transfer(address(refAddress), fees/10);
+
+        // send the winner the rest of the pot
+
+        //initate the random number request
+        Chainlink(vrfCoordinator).requestRandomWords(
+        s_keyHash,
+        s_subscriptionId,
+        requestConfirmations,
+        callbackGasLimit,
+        numWords
+       );
+
+       // if this request fails for a reason such as the winner being banned from receiving USDT, 
+       // forceDrawWinner() can be used to force restart the raffle
+    
+    }
+
+    function forceDrawWinner() public requireOpen onlyAdmin{
+
+        require(rolling, "roll not in progress");
+
+        USDT.transfer(taxWallet[3], USDT.balanceOf(address(this)));
+
+        // Start a new raffle
+
+        raffleNonce ++;
+        nonce = 0;
+        endTime[raffleNonce] = block.timestamp + raffleDuration;
+        rolling = false;
+    }
+
+
+//////////////////////////                                                              /////////////////////////
+/////////////////////////                                                              //////////////////////////
+////////////////////////      Internal and external functions this contract has:      ///////////////////////////
+///////////////////////                                                              ////////////////////////////
+//////////////////////                                                              /////////////////////////////
+
+    // Put internal functions here: (There should NOT be any msg.sender or tx.origin in any function marked with internal)
+
+
+    function rawFulfillRandomWords(uint256 requestId, uint256[] memory randomWords) external {
+
+        require(msg.sender == vrfCoordinator, "Not Authorized");
+        
+        fulfillRandomWords(requestId, randomWords);
+    }
+
+    function fulfillRandomWords(uint256 requestId, uint256[] memory randomWords) internal {
+
+        requestId; // Tell remix I am aware of this variable not being used
+
+        // roll the winner
+
+        uint256 randomValue = (randomWords[0] % (nonce + 1));
+        winner[raffleNonce] = list[raffleNonce][randomValue];
 
         // send the winner the rest of the pot
 
@@ -155,21 +270,7 @@ contract RepeatingLottery{
         raffleNonce ++;
         nonce = 0;
         endTime[raffleNonce] = block.timestamp + raffleDuration;
-    
-    }
-
-
-//////////////////////////                                                              /////////////////////////
-/////////////////////////                                                              //////////////////////////
-////////////////////////      Internal and external functions this contract has:      ///////////////////////////
-///////////////////////                                                              ////////////////////////////
-//////////////////////                                                              /////////////////////////////
-
-    // Put internal functions here: (There should NOT be any msg.sender or tx.origin in here)
-
-    function rollWinner() internal returns(address){
-
-
+        rolling = false;
     }
 
 
@@ -179,25 +280,11 @@ contract RepeatingLottery{
 ///////////////////////                                                              ////////////////////////////
 //////////////////////                                                              /////////////////////////////
 
+
     function pot(uint raffleID) public view returns(uint){
 
         return (internalPot[raffleID]/100)*70; // The pot is equal to the funds collected with 30% taken as fees
     }
-
-
-    
-///////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-//// Additional functions that are not part of the core functionality, if you add anything, please add it here ////
-///////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-
-/*
-    function something() public {
-
-        blah blah blah blah;
-    }
-*/
-
-
 
 }
     
@@ -214,7 +301,11 @@ interface ERC20{
     function decimals() external view returns (uint8);
 }
 
-interface Referral{
+interface Ref{
 
+    function enterReferral(address, uint) external;
+}
 
+interface Chainlink{
+    function requestRandomWords(bytes32 keyHash, uint64 subId, uint16 minimumRequestConfirmations, uint32 callbackGasLimit, uint32 numWords) external returns (uint256 requestId);
 }
